c7b6ce7e6844a521824e2378d3106a04acf5bea9
David Castelblanco <cbdavides@gmail.com>
2020-12-08 17:49:20 -0500

    added a function to delete nodes from de bst

--- bst.hs
+++ bst.hs
@@ -1,27 +1,58 @@
-data Node = Node { value :: Int
-                 , left  :: Maybe Node
-                 , right :: Maybe Node
-                 } deriving (Show)
+data Node = Null | Node { value :: Int, left  :: Node, right :: Node } deriving (Eq, Show)
 
 
-insert :: Maybe Node -> Int -> Node
-insert Nothing val = Node { value = val, left = Nothing, right = Nothing }
-insert (Just node) val = if val <= (value node) then 
-                            Node { value = (value node)
-                                 , left = Just (insert (left node) val)
-                                 , right = (right node)
-                                 }
-                         else 
-                            Node { value = (value node)
-                                 , left = (left node)
-                                 , right = Just (insert (right node) val) 
-                                 }
+insert :: Node -> Int -> Node
+insert Null val = Node { value = val, left = Null, right = Null}
+insert node val = if val == (value node) then node
+                  else if val < (value node) then
+                     Node { value = value node
+                          , left = insert (left node) val
+                          , right = right node
+                          }
+                  else
+                     Node { value = value node
+                          , left = left node
+                          , right = insert (right node) val
+                          }
 
-search :: Maybe Node -> Int -> Maybe Node
-search Nothing _ = Nothing
-search (Just node) val = if val == (value node) then Just node
-                         else if val < (value node) then search (left node) val
-                         else search (right node) val
+
+search :: Node -> Int -> Node
+search Null _ = Null
+search node val = if val == (value node) then node
+                 else if val < (value node) then search (left node) val
+                 else search (right node) val
+
+
+delete :: Node -> Int -> Node
+delete Null _ = Null
+delete node val = if val == (value node) then
+                     replace node
+                  else if val < (value node) then
+                     Node { value = (value node)
+                          , left = delete (left node) val
+                          , right = right node
+                          }
+                  else
+                     Node { value = value node
+                          , left = left node
+                          , right = delete (right node) val
+                          }
+
+
+replace :: Node -> Node
+replace Node { left = Null, right = Null } = Null
+replace Node { left = Null, right = rightChild } = rightChild
+replace Node { left = leftChild, right = Null } = leftChild
+replace node = Node { value = findSuccessor (right node)
+                    , left = left node
+                    , right = delete (right node) (findSuccessor (right node))
+                    }
+
+
+findSuccessor :: Node -> Int
+findSuccessor Node { value = val, left = Null } = val
+findSuccessor Node { left = leftChild } = findSuccessor leftChild
+
 
 main :: IO ()
-main = putStrLn ( show ( search (Just (insert (Just (insert Nothing 10)) 2)) 10 ) )
+main = putStrLn (show  (search (insert (insert Null 10) 2) 10))
